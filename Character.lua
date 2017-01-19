db6b1b2eba99ab68e8fde7bbbd712fa040250f9f
= <=>
2017-01-19 16:51:39 +0100

    break out Player logic from Character

--- Character.lua
+++ Character.lua
@@ -95,52 +95,6 @@ local self = {}
     
     end
     
-    ----------------------------------------------------- UPDATE PLAYER
-    
-    self.updatePlayer = function(dt)   --returns bool of a movement key being down
-
-        ---- Update animation
-        for i=1, #(self.currentAnimation) do
-            self.currentAnimation[i]:update(dt)
-	    end
-        
-        ---- Update character variables
-        --self.x, self.y = self.physics.body:getX(), self.physics.body:getY()
-        --intPlayerVelX, intPlayerVelY = self.physics.body:getLinearVelocity()
-
-        ---- Keyboard RIGHT 
-        if love.keyboard.isDown("right") then
-            self.x = self.x + intPlayerSpeed
-            self.currentAnimation = self.animations["wiggle"]
-
-        ---- Keyboard LEFT
-        elseif love.keyboard.isDown("left") then
-            self.x = self.x - intPlayerSpeed
-            self.currentAnimation = self.animations["wiggle"]
-
-        ---- Keyboard UP
-        elseif love.keyboard.isDown("up") then
-            self.y = self.y - intPlayerSpeed
-            self.currentAnimation = self.animations["wiggle"]
-
-        ---- Keyboard DOWN
-        elseif love.keyboard.isDown("down") then 
-            self.y = self.y + intPlayerSpeed
-            self.currentAnimation = self.animations["wiggle"]
-
-        -- TODO: combined states (up/right, down/left, etc)
-
-        -- NO KEYBOARD
-        else
-           movementKeyDown = false
-           self.currentAnimation = self.animations["idle"]
-        end
-
-    --return movementKeyDown
-    
-
-    end -- End update player
-
     ----------------------------------------------------- DRAW CHARACTER
     self.draw = function()
 
