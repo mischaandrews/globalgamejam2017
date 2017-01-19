6dff2cb876767f97b281c2692e697d505f288b89
= <=>
2017-01-19 17:37:28 +0100

    moving more instance methods out of constructor

--- Character.lua
+++ Character.lua
@@ -132,75 +132,62 @@ local self = {}
 	    end
     
     end
-    
-    ----------------------------------------------------- UPDATE PLAYER
-    
-    self.updatePlayer = function(dt)   --returns bool of a movement key being down
 
-        ---- Update animation
-        for i=1, #(self.currentAnimation) do
-            self.currentAnimation[i]:update(dt)
-	    end
-        
-        ---- Update character variables
-        --self.x, self.y = self.physics.body:getX(), self.physics.body:getY()
-        --intPlayerVelX, intPlayerVelY = self.physics.body:getLinearVelocity()
-
-        ---- Keyboard RIGHT 
-        if love.keyboard.isDown("right") then
-            self.x = self.x + intPlayerSpeed
-            self.currentAnimation = self.animations["walk"]
-
-        ---- Keyboard LEFT
-        elseif love.keyboard.isDown("left") then
-            self.x = self.x - intPlayerSpeed
-            self.currentAnimation = self.animations["walk"]
-
-        ---- Keyboard UP
-        elseif love.keyboard.isDown("up") then
-            self.y = self.y - intPlayerSpeed
-            self.currentAnimation = self.animations["walk"]
-
-        ---- Keyboard DOWN
-        elseif love.keyboard.isDown("down") then 
-            self.y = self.y + intPlayerSpeed
-            self.currentAnimation = self.animations["walk"]
-
-        -- TODO: combined states (up/right, down/left, etc)
-
-        -- NO KEYBOARD
-        else
-           movementKeyDown = false
-           self.currentAnimation = self.animations["idle"]
-        end
-
-    --return movementKeyDown
-    
-
-    end -- End update player
-
-    ----------------------------------------------------- DRAW CHARACTER
-    self.draw = function()
-
-        love.graphics.setColor(255, 255, 255)
-        --self.currentAnim:draw(self.x, self.y, 0, 1, 1, self.width / 2, self.height / 2)
-        
-        for i=1, #(self.currentAnimation) do
-            self.currentAnimation[i]:draw(self.x, self.y, 0, self.scale, self.scale, self.width / 2, self.height / 2)
-	    end
-        
-        -- Variable reference:
-        -- position (x), position (y), ?, scale (x), scale (y), offset X, offsetY
-        -- TODO: check this offset. It might need to be scaled too.
-        
-    end -- End draw
-
-    
-    ----------------------------------------------------- HELPERS
-    
------------------------------------------------------
+    -----------------------------------------------------
 
     return self
+end
+
+function Character:update(dt)
+    ---- Update animation
+    for i=1, #(self.currentAnimation) do
+        self.currentAnimation[i]:update(dt)
+    end
+        
+    ---- Update character variables
+    --self.x, self.y = self.physics.body:getX(), self.physics.body:getY()
+    --intPlayerVelX, intPlayerVelY = self.physics.body:getLinearVelocity()
+
+    ---- Keyboard RIGHT 
+    if love.keyboard.isDown("right") then
+        self.x = self.x + intPlayerSpeed
+        self.currentAnimation = self.animations["walk"]
+
+    ---- Keyboard LEFT
+    elseif love.keyboard.isDown("left") then
+        self.x = self.x - intPlayerSpeed
+        self.currentAnimation = self.animations["walk"]
+
+    ---- Keyboard UP
+    elseif love.keyboard.isDown("up") then
+        self.y = self.y - intPlayerSpeed
+        self.currentAnimation = self.animations["walk"]
+
+    ---- Keyboard DOWN
+    elseif love.keyboard.isDown("down") then 
+        self.y = self.y + intPlayerSpeed
+        self.currentAnimation = self.animations["walk"]
+
+    -- TODO: combined states (up/right, down/left, etc)
+
+    -- NO KEYBOARD
+    else
+        movementKeyDown = false
+        self.currentAnimation = self.animations["idle"]
+    end
+end
+
+function Character:draw()
+    love.graphics.setColor(255, 255, 255)
+    --self.currentAnim:draw(self.x, self.y, 0, 1, 1, self.width / 2, self.height / 2)
+
+    for i=1, #(self.currentAnimation) do
+        self.currentAnimation[i]:draw(self.x, self.y, 0, self.scale, self.scale, self.width / 2, self.height / 2)
+    end
+
+    -- Variable reference:
+    -- position (x), position (y), ?, scale (x), scale (y), offset X, offsetY
+    -- TODO: check this offset. It might need to be scaled too.
 end
 
 function Character:getPosition()
