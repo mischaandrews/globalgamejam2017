f134e34fcf7e2c8c6f2179d69be938b029444ff7
mischaandrews <mischa.andrews@gmail.com>
2017-01-19 15:10:04 +0100

    Interface and mouse files

---
+++ Interface.lua
@@ -0,0 +1,48 @@
+Interface = {
+    
+}
+
+function Interface:new()
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+
+
+
+
+----------------------------------------------------- LOAD
+function Interface:load()
+    
+---------------
+end -- End load
+
+
+
+
+
+
+----------------------------------------------------- UPDATE
+function Interface:update(dt)
+    
+    
+-----------------
+end -- End update
+
+
+
+
+
+----------------------------------------------------- DRAW
+
+function Interface:draw()
+
+    
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.print("test")
+
+    
+---------------
+end -- End draw
---
+++ Mouse.lua
@@ -0,0 +1,10 @@
+-- To include:
+-- love.mouse.getCursor
+-- love.mouse.getPosition
+-- love.mouse.getX
+-- love.mouse.getY
+-- love.mouse.isDown
+-- love.mouse.isGrabbed
+-- love.mousepressed
+-- love.mousereleased
+-- love.mousemoved
