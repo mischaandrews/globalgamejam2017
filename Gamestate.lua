fca9ca713d19e5df4f9a79002911f55b947043c6
= <=>
2017-01-19 15:09:16 +0100

    encapsulate physics

--- Gamestate.lua
+++ Gamestate.lua
@@ -7,7 +7,8 @@ require "Camera"
 
 
 Gamestate = {
-    camera
+    camera,
+    physics
 }
 
 function Gamestate:new()
@@ -36,17 +37,18 @@ function Gamestate:load()
     intWindowY = 800
     
     ---- Load level
-    world = love.physics.newWorld(0, 0, true) --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 0
-    world:setCallbacks(beginContact)
+    local physics = love.physics.newWorld(0, 0, true)
+    physics:setCallbacks(beginContact)
+    self.physics = physics
    
     ---- Create characters
     player1_spawnX = 150
     player1_spawnY = 200
-    player1 = character.new(world, player1_spawnX, player1_spawnY, "pink", "playable")
+    player1 = character.new(physics, player1_spawnX, player1_spawnY, "pink", "playable")
     
     npc1_spawnX = 350
     npc1_spawnY = 250
-    npc1 = character.new(world, npc1_spawnX, npc1_spawnY, "blue", "npc")
+    npc1 = character.new(physics, npc1_spawnX, npc1_spawnY, "blue", "npc")
     
     ---- Initial graphics setup
 	--love.graphics.setMode(intWindowX, intWindowY)
@@ -86,7 +88,7 @@ function Gamestate:update(dt)
     
         love.graphics.print("test")
 
-        world:update(dt) --this puts the world into motion
+        self.physics:update(dt) --this puts the world into motion
 
         ---- Update camera
         playerX, playerY = player1.getPosition()
