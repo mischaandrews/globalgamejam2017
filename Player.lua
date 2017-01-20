1cf920a395beeb4f5e67e2b8588ecd3342224f6b
= <=>
2017-01-20 21:06:19 +0100

    changing how animations are loaded

--- Player.lua
+++ Player.lua
@@ -3,7 +3,8 @@ require "Animation"
 Player = {
     x,
     y,
-    animation
+    animations,
+    currentAnimation
 }
 
 function Player:new()
@@ -13,22 +14,16 @@ function Player:new()
     return o
 end
 
-function Player:load(characterSprite)
-    local animation = Animation:new()
-    animation:load(characterSprite)
-    self.animation = animation
+function Player:load(world, x, y, characterSprite)
+    local currentAnimation = Animation:new()
+    currentAnimation:load(characterSprite)
+    self.currentAnimation = currentAnimation
 end
 
 function Player:update(dt)
 
     ---- Update animation
-    for i=1, #(self.currentAnimation) do
-        self.currentAnimation[i]:update(dt)
-    end
-
-    ---- Update character variables
-    --self.x, self.y = self.physics.body:getX(), self.physics.body:getY()
-    --intPlayerVelX, intPlayerVelY = self.physics.body:getLinearVelocity()
+    --self.currentAnimation:update(dt)
 
     ---- Keyboard RIGHT 
     if love.keyboard.isDown("right") then
@@ -55,20 +50,11 @@ function Player:update(dt)
     -- NO KEYBOARD
     else
         movementKeyDown = false
-        self.currentAnimation = self.animations["idle"]
+        --self.currentAnimation = self.animations["idle"]
     end
 
 end
 
 function Player:draw()
-
-    --love.graphics.setColor(255, 255, 255, 255)
-    --self.currentAnim:draw(self.x, self.y, 0, 1, 1, self.width / 2, self.height / 2)
-
-    --self.currentAnimation[i]:draw(self.x, self.y, 0, self.scale, self.scale, self.width / 2, self.height / 2)
-
-    -- Variable reference:
-    -- position (x), position (y), ?, scale (x), scale (y), offset X, offsetY
-    -- TODO: check this offset. It might need to be scaled too.
 
 end
