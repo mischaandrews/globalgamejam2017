d57b422f06ce5226ea95bb94d004d2e24bba8d3f
= <=>
2017-01-21 14:04:45 +0100

    removed what i think is dead code

--- Pickup.lua
+++ Pickup.lua
@@ -37,52 +37,15 @@ function Pickup:load(world, x, y, pickupSprite)
 
 end
 
-
 function Pickup:update(dt)
     self.currentAnimation:update(dt)
 end
 
 function Pickup:draw()
-
     love.graphics.setColor(255, 255, 255)
-
     love.graphics.rectangle("line", self.x-3, self.y-3, 106,106)
     self.currentAnimation:draw(self.x, self.y, self.scale)
-
     -- todo: load width and height properly
-    
-    --for i=1, #(self.animation) do
-        --self.animation[i]:draw(self.x, self.y, 0, self.scale, self.scale, 256 / 2, 256 / 2)
-    --end
-    -- todo: load width and height properly
-
-
-    --self.animation:draw(self.x,self.y)
-end
-
-function Pickup:getPosition()
-    return self.x, self.y
-end
-
-function Pickup:getVelocity()
-    return self.physics.body:getLinearVelocity()
-end
-
-function Pickup:getTotalVelocity()
-    local vx, vy = self.physics.body:getLinearVelocity()
-    return math.sqrt(vx*vx + vy*vy)
-end
-
-function Pickup:applyForce(fx,fy)
-    self.physics.body:applyForce(fx,fy)
-end
-
-function Pickup:setMass(mass)
-    self.physics.body:setMass(mass)
-end
-
-function Pickup:getPhysics()
-    return self.physics
 end
 
 function Pickup:pickupSprite()
