563b83b6f2a0697501fda52a5f77f69af2b51a13
= <=>
2017-01-22 11:30:51 +0100

    octopus physics

--- Octopus.lua
+++ Octopus.lua
@@ -38,22 +38,47 @@ function Octopus:load(world, x, y, characterSprite)
     self.animations = Animation.loadAnimations(characterSprite, {"idle", "grab"}, self.spriteLayerNames)
     self.currentAnimations = self.animations["idle"]
 
-    self.physics = {}
-    --remember, the shape (the rectangle we create next) anchors to the body from its center
-    self.physics.body = love.physics.newBody(world, self.x, self.y, "dynamic") 
+    self.physics = self:loadPhysics(world, x, y)
 
 end
 
-function Octopus:spawn(physics, player, map)
+function Octopus:loadPhysics(world, x, y)
+    local physics = {}
+    physics.body = love.physics.newBody(world, x, y, "dynamic")
+    physics.shape = love.physics.newCircleShape(octopusRadius)
+    physics.fixture = love.physics.newFixture(physics.body, physics.shape, 1)
+    physics.fixture:setUserData({"player",self})
+    physics.fixture:setRestitution(0.8)
+    return physics
+end
+
+function Octopus:spawn(world, player, map)
     local npc2_spawnX = 450
     local npc2_spawnY = 450
     local npc2 = Octopus:new()
-    npc2:load(physics, npc2_spawnX, npc2_spawnY, "octopus")
+    npc2:load(world, npc2_spawnX, npc2_spawnY, "octopus")
     return npc2
 end
 
 function Octopus:update(dt)
-  -- self.currentAnimations:update(dt, self.spriteLayerNames)
+    self:updateMovement()
+    self:updateAnimation(dt)
+end
+
+function Octopus:updateMovement()
+
+    local forceX, forceY = getOceanForce(self)
+
+    --Pass the force to the physics engine
+    self.physics.body:applyForce(forceX, forceY)
+
+    --Get the position back out of the physics engine
+    --From last frame, but whatever
+    self.x, self.y = self.physics.body:getPosition()
+
+end
+
+function Octopus:updateAnimation(dt)
     for i=1, #self.spriteLayerNames do
         self.currentAnimations[self.spriteLayerNames[i]]:update(dt, self.spriteLayerNames)
     end
